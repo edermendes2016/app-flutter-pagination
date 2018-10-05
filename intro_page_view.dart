3b4bbe28875e76fdefd905f04eda92a7041ae8b4
edermendes2016 <ederkbb@gmail.com>
2018-10-05 11:43:15 -0400

    Add files via upload

---
+++ intro_page_view.dart
@@ -0,0 +1,36 @@
+import 'package:app_transformer/data.dart';
+import 'package:app_transformer/intro_page_item.dart';
+import 'package:app_transformer/page_transformer.dart';
+import 'package:flutter/material.dart';
+
+
+class IntroPageView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: SizedBox.fromSize(
+          size: const Size.fromHeight(500.0),
+          child: PageTransformer(
+            pageViewBuilder: (context, visibilityResolver) {
+              return PageView.builder(
+                controller: PageController(viewportFraction: 0.85),
+                itemCount: sampleItems.length,
+                itemBuilder: (context, index) {
+                  final item = sampleItems[index];
+                  final pageVisibility =
+                      visibilityResolver.resolvePageVisibility(index);
+
+                  return IntroPageItem(
+                    item: item,
+                    pageVisibility: pageVisibility,
+                  );
+                },
+              );
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
